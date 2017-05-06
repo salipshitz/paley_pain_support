f59406ad8d13518c7d67c6f78e863fde7d941a4e
refstaff <beltib@Deborahs-MacBook-Pro.local>
2017-05-05 18:10:04 -0700

    Initial commit

--- Paley Pain Support/FirstViewController.swift
+++ Paley Pain Support/FirstViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    
+    @IBOutlet weak var painAmount: UISlider!{
+        didSet{
+            painAmount.transform = CGAffineTransform(rotationAngle: CGFloat(3 * Double.pi / 2))
+        }
+    }
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    var pain = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +29,11 @@ class FirstViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func SliderSlid(_ sender: UISlider) {
+        pain = Int(round(sender.value))
+        numberLabel.text = String(pain)
+    }
 
 }
 
